b6e9fc81b393fd04b1080b950b06c67314574ec5
“Joshua <umukorojosh@gmail.com>
2022-05-28 22:34:47 +0100

    Enter

---
+++ Finding-Anagrams/main.py
@@ -0,0 +1,14 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if(sorted(word) != sorted(anagram)):
+       return False
+    else:
+        return True
+
+find_anagram("hello", "check")
